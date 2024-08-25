c5876984bc4d4a8df0473f9dc87965230d3fa55d
Sumit Kumar <49060256+sumitchaudhary11@users.noreply.github.com>
2024-08-25 13:27:42 +0530

    Update _app.tsx

--- weather_information_application/src/pages/_app.tsx
+++ weather_information_application/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from '../store/store'; // Adjust the import path if necessary
+import store from '../store/store';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: any) => {
